9a011bde09c62154a6736b6ea27d215f39367ceb
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-17 10:43:34 +0200

    done lesson 4

--- myApp.js
+++ myApp.js
@@ -31,7 +31,13 @@ const createAndSavePerson = done => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      done(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findPeopleByName = (personName, done) => {
