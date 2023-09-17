a2e260fd2a1cb4b2adbc6c07bb2c7717533b1572
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-17 11:29:17 +0200

    done lesson 12

--- myApp.js
+++ myApp.js
@@ -128,8 +128,16 @@ const removeManyPeople = done => {
 
 const queryChain = done => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: 1 })
+    .limit(2)
+    .select("-name")
+    .exec((err, data) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, data);
+    });
 };
 
 /** **Well Done !!**
