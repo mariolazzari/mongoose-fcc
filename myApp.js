33b85b248e2be4a83fd5c47bf60b9db619e27a8e
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-17 11:08:13 +0200

    done lesson 7

--- myApp.js
+++ myApp.js
@@ -30,7 +30,7 @@ const createAndSavePerson = done => {
   });
 };
 
-var arrayOfPeople = [
+const arrayOfPeople = [
   { name: "Mario", age: 48, favoriteFoods: ["Pizza"] },
   { name: "Mariarosa", age: 46, favoriteFoods: ["Pizza"] },
   { name: "Roberto", age: 74, favoriteFoods: ["Pizza"] },
@@ -67,7 +67,13 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) {
+      done(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findEditThenSave = (personId, done) => {
