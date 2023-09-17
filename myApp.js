d3fbe5b2e421df50d2b2cc650d992b28c5fe6600
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-17 10:24:48 +0200

    done lesson 2

--- myApp.js
+++ myApp.js
@@ -6,7 +6,13 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
 
-let Person;
+const PersonSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  age: { type: Number },
+  foods: { type: [String] },
+});
+
+let Person = mongoose.model("Person", PersonSchema);
 
 const createAndSavePerson = done => {
   done(null /*, data*/);
