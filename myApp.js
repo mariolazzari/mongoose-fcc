bc1f79566988da40d5508f1fc14d23c0ab615977
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-17 08:34:23 +0200

    done lesson 1

--- myApp.js
+++ myApp.js
@@ -1,4 +1,12 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+console.log("URI", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
