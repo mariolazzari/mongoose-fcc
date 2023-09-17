59f8f59799ecf9325f8421dcdda0b346a131e61f
Mario Lazzari <mario.lazzari@gmail.com>
2023-09-17 11:04:18 +0200

    done lesson 6

--- myApp.js
+++ myApp.js
@@ -57,7 +57,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne(food, (err, data) => {
+    if (err) {
+      done(err);
+    } else {
+      done(null, data);
+    }
+  });
 };
 
 const findPersonById = (personId, done) => {
